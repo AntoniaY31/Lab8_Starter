8639e1633f62ad2f2c831555ec2df733498528d6
Antonia Manshi Yang <may003@ucsd.edu>
2021-11-14 15:53:03 -0800

    Complete part2

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 
 1. Ans: I would fit my automated tests within a Github action so that it runs whenever the code get pushed. By doing this, we can guarantee that every attempt of push gets tested automatically which prevents the overall project get carshed if someone acccidentally push some errored code to the repository.
 2. Ans: No. It's better to do unit testing to test functions.
-3. Ans:
-4. Ans:
+3. Ans: No. Unit testing can help us debug on a small sccale without moving many parts. However, message feature may involves many parts so that it is hard to do unit testing.
+4. Ans: Yes. This “max message length” feature seems to be able to tested without involving too many parts since we only need to change the message length. Thus it is plausible to do unit testing on it.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,97 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// Test for isPhoneNumber
+// expect true
+test("valid number1", ()=>{
+    expect(functions.isPhoneNumber("858-268-7960")).toBe(true);
+});
+
+test("valid number2", ()=>{
+    expect(functions.isPhoneNumber("123-200-7960")).toBe(true);
+});
+
+// expect false
+test("invalid number1", ()=>{
+    expect(functions.isPhoneNumber("85A2687960")).toBe(false);
+});
+
+test("invalid number2", ()=>{
+    expect(functions.isPhoneNumber("2687960")).toBe(false);
+});
+
+// Test for isEmail
+// expect true
+test("valid email1", ()=>{
+    expect(functions.isEmail("antoniayang31@gmail.com")).toBe(true);
+});
+
+test("valid email2", ()=>{
+    expect(functions.isEmail("may003@ucsd.edu")).toBe(true);
+});
+
+// expect false
+test("invalid email1", ()=>{
+    expect(functions.isEmail("858-2687960")).toBe(false);
+});
+
+test("invalid number2", ()=>{
+    expect(functions.isEmail("may003")).toBe(false);
+});
+
+// Test for isStrongPassword
+// expect true
+test("valid strong password1", ()=>{
+    expect(functions.isStrongPassword("antoniayang31")).toBe(true);
+});
+
+test("valid strong password2", ()=>{
+    expect(functions.isStrongPassword("antoniaya___")).toBe(true);
+});
+
+// expect false
+test("invalid strong password1", ()=>{
+    expect(functions.isStrongPassword("an")).toBe(false);
+});
+
+test("invalid strong password2", ()=>{
+    expect(functions.isStrongPassword("antoniayang31@gmail.com")).toBe(false);
+});
+
+// Test for isDate
+// expect true
+test("valid Date1", ()=>{
+    expect(functions.isDate("7/31/2001")).toBe(true);
+});
+
+test("valid Date2", ()=>{
+    expect(functions.isDate("12/1/2001")).toBe(true);
+});
+
+// expect false
+test("invalid Date1", ()=>{
+    expect(functions.isDate("100/2000/2001")).toBe(false);
+});
+
+test("invalid Date2", ()=>{
+    expect(functions.isDate("2001")).toBe(false);
+});
+
+// Test for isHexColor
+// expect true
+test("valid Hex1", ()=>{
+    expect(functions.isHexColor("#FFFFFF")).toBe(true);
+});
+
+test("valid Hex2", ()=>{
+    expect(functions.isHexColor("#000000")).toBe(true);
+});
+
+// expect false
+test("invalid Hex1", ()=>{
+    expect(functions.isHexColor("#FF")).toBe(false);
+});
+
+test("invalid Hex2", ()=>{
+    expect(functions.isHexColor("#ZZZZZZ")).toBe(false);
+});
